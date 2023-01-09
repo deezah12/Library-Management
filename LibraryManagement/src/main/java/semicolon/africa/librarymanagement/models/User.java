b01e7341b2061b80c0f8f6c551c381aebf17b9a4
Hadiza <deezah30@gmail.com>
2023-01-09 13:26:43 +0100

    user entity with name, id, email, password, and book,category,publisher,author references

---
+++ LibraryManagement/src/main/java/semicolon/africa/librarymanagement/models/User.java
@@ -0,0 +1,18 @@
+package semicolon.africa.librarymanagement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@AllArgsConstructor
+public class User {
+
+    @Id private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
